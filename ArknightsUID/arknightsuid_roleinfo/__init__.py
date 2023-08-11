b3f36cf0baabcac29967d6f1a08ea4e6fae66591
qwerdvd <105906879+qwerdvd@users.noreply.github.com>
2023-08-11 21:33:00 +0800

    🎨添加所需背景素材

--- ArknightsUID/arknightsuid_roleinfo/__init__.py
+++ ArknightsUID/arknightsuid_roleinfo/__init__.py
@@ -16,10 +16,6 @@ sv_get_info = SV('ark查询信息')
 
 @sv_get_info.on_command((f'{PREFIX}uid'))
 async def send_role_info(bot: Bot, ev: Event):
-    # name = ''.join(re.findall('[\u4e00-\u9fa5]', ev.text))
-    # if name:
-    #     return
-
     uid = await get_uid(bot, ev, bind_model=ArknightsBind)
     if uid is None:
         return '你还没有绑定UID噢,请使用[ark绑定uid123]完成绑定!'

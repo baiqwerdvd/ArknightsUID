a398faaa974894e6625342200eb506e973ab870b
qwerdvd <105906879+qwerdvd@users.noreply.github.com>
2023-09-05 11:12:30 +0800

    ✨ Client:2.0.81 Data:23-09-03-22-22-41-2c49c2

--- ArknightsUID/version.py
+++ ArknightsUID/version.py
@@ -1,3 +1,3 @@
 ArknightsUID_version = '0.1.0'
-Arknights_Client_version = '2.0.61'
-Arknights_Res_version = '23-08-25-11-36-41-12f55f'
+Arknights_Client_version = '2.0.81'
+Arknights_Res_version = '23-09-03-22-22-41-2c49c2'

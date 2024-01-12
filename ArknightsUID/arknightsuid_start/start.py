a6a3d80fb511997d75241579cac84b5334d6fc79
qwerdvd <105906879+qwerdvd@users.noreply.github.com>
2024-01-12 13:45:57 +0800

    fix: 另起线程下载资源

--- ArknightsUID/arknightsuid_start/start.py
+++ ArknightsUID/arknightsuid_start/start.py
@@ -1,12 +1,18 @@
 import asyncio
+import threading
+
+from gsuid_core.logger import logger
 
 from ..arknightsuid_resource import startup
 from ..utils.database.startup import ark_adapter
 
 
 async def all_start():
-    await startup()
-    await ark_adapter()
+    try:
+        await startup()
+        await ark_adapter()
+    except Exception as e:
+        logger.exception(e)
 
 
-asyncio.run(all_start())
+threading.Thread(target=lambda: asyncio.run(all_start()), daemon=True).start()

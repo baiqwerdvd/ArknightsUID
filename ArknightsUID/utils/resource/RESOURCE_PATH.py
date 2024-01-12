ee6e4eafd106289fc4df112dfc738315074d686a
qwerdvd <105906879+qwerdvd@users.noreply.github.com>
2024-01-12 13:53:13 +0800

    ✨下载全部资源

--- ArknightsUID/utils/resource/RESOURCE_PATH.py
+++ ArknightsUID/utils/resource/RESOURCE_PATH.py
@@ -9,8 +9,19 @@ CU_BG_PATH = MAIN_PATH / 'bg'
 CONFIG_PATH = MAIN_PATH / 'config.json'
 PLAYER_PATH = MAIN_PATH / 'players'
 RESOURCE_PATH = MAIN_PATH / 'resource'
+
+
 GAMEDATA_PATH = RESOURCE_PATH / 'gamedata'
-SKIN_PATH = RESOURCE_PATH / 'skin'
+
+CHAR_AVATAR_PATH = RESOURCE_PATH / 'char_avatar'
+CHARARTS_PATH = RESOURCE_PATH / 'chararts'
+MEDAL_DIY_FRAME_BKG_PATH = RESOURCE_PATH / 'medal_diy_frame_bkg'
+MEDAL_ICONS_PATH = RESOURCE_PATH / 'medal_icons'
+PLAYER_AVATAR_LIST = RESOURCE_PATH / 'player_avatar_list'
+SKILL_ICONS_PATH = RESOURCE_PATH / 'skill_icons'
+SKINPACK_PATH = RESOURCE_PATH / 'skinpack'
+SUB_PROFESSION_ICON_PATH = RESOURCE_PATH / 'sub_profession_icon'
+TEAM_ICON_PATH = RESOURCE_PATH / 'team_icon'
 
 
 def init_dir():
@@ -20,7 +31,16 @@ def init_dir():
         PLAYER_PATH,
         RESOURCE_PATH,
         GAMEDATA_PATH,
-        SKIN_PATH,
+        SKINPACK_PATH,
+        CHAR_AVATAR_PATH,
+        CHARARTS_PATH,
+        MEDAL_DIY_FRAME_BKG_PATH,
+        MEDAL_ICONS_PATH,
+        PLAYER_AVATAR_LIST,
+        SKILL_ICONS_PATH,
+        SKINPACK_PATH,
+        SUB_PROFESSION_ICON_PATH,
+        TEAM_ICON_PATH,
     ]:
         i.mkdir(parents=True, exist_ok=True)
 
--- ArknightsUID/utils/resource/download_from_cos.py
+++ ArknightsUID/utils/resource/download_from_cos.py
@@ -1,6 +1,17 @@
 from gsuid_core.utils.download_resource.download_core import download_all_file
 
-from .RESOURCE_PATH import GAMEDATA_PATH
+from .RESOURCE_PATH import (
+    CHAR_AVATAR_PATH,
+    CHARARTS_PATH,
+    GAMEDATA_PATH,
+    MEDAL_DIY_FRAME_BKG_PATH,
+    MEDAL_ICONS_PATH,
+    PLAYER_AVATAR_LIST,
+    SKILL_ICONS_PATH,
+    SKINPACK_PATH,
+    SUB_PROFESSION_ICON_PATH,
+    TEAM_ICON_PATH,
+)
 
 
 async def download_all_file_from_cos():
@@ -8,5 +19,14 @@ async def download_all_file_from_cos():
         'ArknightsUID',
         {
             'resource/gamedata': GAMEDATA_PATH,
+            'resource/char_avatar': CHAR_AVATAR_PATH,
+            'resource/chararts': CHARARTS_PATH,
+            'resource/medal_diy_frame_bkg': MEDAL_DIY_FRAME_BKG_PATH,
+            'resource/medal_icons': MEDAL_ICONS_PATH,
+            'resource/player_avatar_list': PLAYER_AVATAR_LIST,
+            'resource/skill_icons': SKILL_ICONS_PATH,
+            'resource/skinpack': SKINPACK_PATH,
+            'resource/sub_profession_icon': SUB_PROFESSION_ICON_PATH,
+            'resource/team_icon': TEAM_ICON_PATH,
         },
     )
